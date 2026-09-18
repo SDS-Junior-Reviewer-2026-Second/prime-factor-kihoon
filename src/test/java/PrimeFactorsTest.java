6538919d19f3ddc29fa44974fbe46bb332b76dd5
KKH4100 <kimrlgns7956@snu.ac.kr>
2026-09-18 10:10:32 +0900

    Initial Version

---
+++ src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,15 @@
+import org.example.PrimeFactors;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PrimeFactorsTest {
+
+	@Test
+	public void test() {
+		PrimeFactors primeFactors = new PrimeFactors();
+		assertEquals(10, 10);
+	}
+
+}
