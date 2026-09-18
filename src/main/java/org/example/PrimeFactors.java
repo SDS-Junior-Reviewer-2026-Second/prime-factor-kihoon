495826671782f233e1897aa865086ee66e5656fd
KKH4100 <kimrlgns7956@snu.ac.kr>
2026-09-18 10:33:26 +0900

    Just One Commit.......

--- src/main/java/org/example/PrimeFactors.java
+++ src/main/java/org/example/PrimeFactors.java
@@ -1,5 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactors {
 
+    public List<Integer> of(int number) {
+        List<Integer> factors = new ArrayList<>();
+
+        for (int divisor = 2; number > 1; divisor++){
+            while (number % divisor == 0) {
+                factors.add(divisor);
+                number /= divisor;
+            }
+        }
+
+        return factors;
+    }
 }
--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,15 +1,54 @@
 import org.example.PrimeFactors;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PrimeFactorsTest {
 
-	@Test
-	public void test() {
-		PrimeFactors primeFactors = new PrimeFactors();
-		assertEquals(10, 10);
+	private PrimeFactors primeFactors;
+
+	@BeforeEach
+	void setUp() {
+		primeFactors = new PrimeFactors();
 	}
 
+	@Test
+	public void testPrimeFactorOf1() {
+
+		assertEquals(Arrays.asList(), primeFactors.of(1));
+	}
+
+	@Test
+	public void testPrimeFactorOf2(){
+		assertEquals(Arrays.asList(2), primeFactors.of(2));
+	}
+
+	@Test
+	public void testPrimeFactorOf3(){
+		assertEquals(Arrays.asList(3), primeFactors.of(3));
+	}
+
+	@Test
+	public void testPrimeFactorOf4(){
+		assertEquals(Arrays.asList(2, 2), primeFactors.of(4));
+	}
+
+	@Test
+	public void testPrimeFactorOf6(){
+		assertEquals(Arrays.asList(2, 3), primeFactors.of(6));
+	}
+
+	@Test
+	public void testPrimeFactorOf9(){
+		assertEquals(Arrays.asList(3, 3), primeFactors.of(9));
+	}
+
+	@Test
+	public void testPrimeFactorOf12(){
+		assertEquals(Arrays.asList(2, 2, 3), primeFactors.of(12));
+	}
 }
